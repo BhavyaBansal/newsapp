a1841b5277f26e3325b9581f7defa5195462bf39
Bhavya Bansal <bhavyabansal0916@gmail.com>
2022-11-22 01:52:36 +0530

    Loader and links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,25 +33,16 @@ export class Navbar extends Component {
                     Home
                   </a>
                 </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Link
+                <li className="nav-item"><a className="nav-link" href="/">About
                   </a>
                 </li>
-                {/* <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><hr className="dropdown-divider"/></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link disabled">Disabled</a>
-                    </li> */}
+                <li className="nav-item"><a className="nav-link" href="/">business</a></li>
+                <li className="nav-item"><a className="nav-link" href="/">entertainment</a></li>
+                <li className="nav-item"><a className="nav-link" href="/">general</a></li>
+                <li className="nav-item"><a className="nav-link" href="/">health</a></li>
+                <li className="nav-item"><a className="nav-link" href="/">science</a></li>
+                <li className="nav-item"><a className="nav-link" href="/">sports</a></li>
+                <li className="nav-item"><a className="nav-link" href="/">technology</a></li>
               </ul>
             </div>
           </div>
--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
-
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 export default class News extends Component {
+  static defaultProps = {
+    country: "in",
+    pageSize: 6,
+    category: "general",
+  };
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
   constructor() {
     super();
     //console.log("construstor runs first"); //runs first
@@ -20,64 +31,88 @@ export default class News extends Component {
   return value, or the rejected value is thrown.*/
   async componentDidMount() {
     // console.log("cdm runs last");
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=dbd17c82f4814c2f9bc31798ad56d963&page=1&pageSize=9";
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dbd17c82f4814c2f9bc31798ad56d963&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url); //returns promise
     let parseddata = await data.json();
     // console.log(parseddata);
     this.setState({
       articles: parseddata.articles,
       totalResults: parseddata.totalResults,
+      loading: false,
     });
   }
   handleNext = async () => {
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 9)) {
+    if (
+      this.state.page + 1 >
+      Math.ceil(this.state.totalResults / this.props.pageSize)
+    ) {
       // document.getElementsByClassName("nextbtn").disabled = true;
     } else {
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=dbd17c82f4814c2f9bc31798ad56d963&page=${
+      let url = `https://newsapi.org/v2/top-headlines?country=${
+        this.props.country
+      }&category=${
+        this.props.category
+      }&apiKey=dbd17c82f4814c2f9bc31798ad56d963&page=${
         this.state.page + 1
-      }&pageSize=9`;
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
       let data = await fetch(url); //returns promise
       let parseddata = await data.json();
       this.setState({
         page: this.state.page + 1,
         articles: parseddata.articles,
+        loading: false,
       });
     }
   };
   handlePrev = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=dbd17c82f4814c2f9bc31798ad56d963&page=${
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${
+      this.props.category
+    }&apiKey=dbd17c82f4814c2f9bc31798ad56d963&page=${
       this.state.page - 1
-    }&pageSize=9`;
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url); //returns promise
     let parseddata = await data.json();
     this.setState({
       page: this.state.page - 1,
       articles: parseddata.articles,
+      loading: false,
     });
   };
   render() {
     // console.log("render method runs second");
     return (
       <div className="container my-3">
-        <h2>News Updates:</h2>
+        {this.state.loading && <Spinner />}
+        <h2 className="text-center" style={{ margin: "30px 0px" }}>
+          News Updates:
+        </h2>
+        {/* <h2>News Updates:</h2> */}
         <div className="row">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title ? element.title.slice(0, 45) : ""}
-                  description={
-                    element.description
-                      ? element.description.slice(0, 60)
-                      : "Nothing to show"
-                  }
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div
+                  className="col-md-4 text-center d-flex justify-content-center"
+                  key={element.url}
+                >
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 60)
+                        : "Nothing to show"
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
@@ -89,6 +124,10 @@ export default class News extends Component {
             &larr; Previous
           </button>
           <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
             type="button"
             className="btn btn-dark my-2 mx-2"
             onClick={this.handleNext}
